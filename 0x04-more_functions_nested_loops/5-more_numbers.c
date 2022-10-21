b0089c72a2b9dd69e22b2f5cae58350f91fe96a8
cuzondu25 <cuzondu25@gmail.com>
2022-10-20 18:54:34 -0700

    nested loop function

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times the numbers
+ */
+void more_numbers(void);
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 15; j++)
+			_putchar(j + '0');
+	}
+	_putchar('\n');
+}
