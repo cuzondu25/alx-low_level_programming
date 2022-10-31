c9f9ef769130cc41df84f52cbd94d88613650001
cuzondu25 <cuzondu25@gmail.com>
2022-10-31 13:50:20 -0700

    even more on pointer, arrays and strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - locate a character in a string
+ * @s: string
+ * @c: character to be located
+ * Return: c
+ */
+char *_strchr(char *s, char c)
+{
+	if (*s == NULL)
+		return (NULL);
+	for (; *s; s++)
+	{
+		if (*s == c)
+			return (s);
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (NULL);
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - print the sum of two diagonals of a square matrix
+ * @a: the matrix
+ * @size: size of matrix
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, sum1, sum2;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	for (i = 0; i < size * size; i++)
+	{
+		if (i % (size + 1) == 0)
+		{
+			sum1 += a[i];
+		}
+		if (i % (size - 1) == 0 && (i > 0 && (i < (size * size - 1))))
+		{
+			sum2 += a[i];
+		}
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
